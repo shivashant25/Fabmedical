a886b801c7efb1996d85f3335d0786d426372f59
shivashant25 <smm.yzfr1@gmail.com>
2022-06-14 03:03:48 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("0b9076aa-466a-4b71-befc-e9f7015717ac");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
